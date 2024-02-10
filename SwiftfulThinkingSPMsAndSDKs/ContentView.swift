3a7e8cc9a9cb94d8146336b0b69ff8d5952823df
Nick <hello@swiftful-thinking.com>
2024-02-09 19:39:46 -0500

    Initial Commit

---
+++ SwiftfulThinkingSPMsAndSDKs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingSPMsAndSDKs
+//
+//  Created by Nick Sarno on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingSPMsAndSDKs/SwiftfulThinkingSPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingSPMsAndSDKsApp.swift
+//  SwiftfulThinkingSPMsAndSDKs
+//
+//  Created by Nick Sarno on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingSPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
